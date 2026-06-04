9e0cc616cab215c24eaebc4e8817d3856488f626
Angela <angelaayodeji@gmail.com>
2026-06-04 15:58:10 -0500

    completed memory game...maybe add more like loading screen in the future

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   // const [data, setData] = useState([]);
   const [charList, setCharList] = useState([]);
   const [loading, setLoading] = useState([false]);
+  const [bestScore, setBestScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
 
   function shuffleList(listOfChar) {
     return [...listOfChar].sort(() => Math.random() - 0.5);
@@ -17,8 +19,8 @@ function App() {
           "https://dattebayo-api.onrender.com/characters/1344,1307,437,374,376,259,739,1293,1303,1280,928,861,1365,444,1216",
         );
         const data = await response.json();
-        console.log(data);
-        setCharList(shuffleList(data));
+        const cards = data.map((item) => ({ ...item, isClicked: false }));
+        setCharList(shuffleList(cards));
       } catch (error) {
         console.error("Error:", error);
       }
@@ -27,25 +29,55 @@ function App() {
     getData();
   }, []);
 
+  if(currentScore ==15){
+    return(
+      <div className="appContainer">
+        <h1>YOU WIN</h1>
+        <h1>YOU WIN</h1>
+        <h1>YOU WIN</h1>
+        <h1>YOU WIN</h1>
+        </div>
+      
+    )
+  }
+
   if (loading) {
     return (
-      <>
+      <div className="appContainer">
         <div>...Loading</div>
-      </>
+      </div>
     );
   }
 
   return (
-    <>
-      {charList.map((char) => (
-        <Card
-          key={char.id}
-          name={char.name}
-          imgURL={char.images[0]}
-          onClick={() => setCharList(shuffleList(charList))}
-        />
-      ))}
-    </>
+    <div className="appContainer">
+      <h2>Current: {currentScore}</h2>
+      <h2>Best: {bestScore}</h2>
+      <div className="cardContainer">
+        {charList.map((char) => (
+          <Card
+            key={char.id}
+            name={char.name}
+            imgURL={char.images[0]}
+            onClick={() => {
+              setCharList(shuffleList(charList));
+              if (char.isClicked) {
+                setCharList((prev) =>
+                  prev.map((char) => ({ ...char, isClicked: false })),
+                );
+                if (currentScore > bestScore) {
+                  setBestScore(currentScore);
+                }
+                setCurrentScore(0);
+                return;
+              }
+              char.isClicked = true;
+              setCurrentScore(currentScore + 1);
+            }}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import "../styling/Card.css";
 export default function Card({ name, imgURL, onClick }) {
   return (
-    <div className="cardContainer" onClick={onClick}>
+    <div className="card" onClick={onClick}>
       <img src={imgURL} alt={name}></img>
       <h1>{name}</h1>
     </div>
